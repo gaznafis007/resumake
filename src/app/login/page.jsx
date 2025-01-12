b885169d88039d48a9fc7a5833a52f37c54ad10c
unknown <gazinafisrafi.gnr@gmail.com>
2025-01-12 16:04:18 +0600

    login and register init

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-4xl text-center text-purple-800 font-semibold">This is login</h2>
+        </div>
+    );
+};
+
+export default Login;
